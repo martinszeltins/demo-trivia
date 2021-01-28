ac5a70611f18223ed23db61bc9f321df5b311868
Martins <martinszeltins@gmail.com>
2021-01-28 17:21:03 +0200

    Refactoring

--- server/app/Http/Controllers/QuestionController.php
+++ server/app/Http/Controllers/QuestionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 
-class QuestionAndAnswersController extends Controller
+class QuestionController extends Controller
 {
     /**
      * Return a random question with possible answers
--- server/routes/api.php
+++ server/routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\NewGameController;
-use App\Http\Controllers\QuestionAndAnswersController;
+use App\Http\Controllers\QuestionController;
 
-Route::get('question',     [QuestionAndAnswersController::class, 'show']);
+Route::get('question',     [QuestionController::class, 'show']);
 Route::get('answer',       [AnswerController::class, 'show']);
 Route::post('new-game',    [NewGameController::class, 'store']);
